4d4ed8043d5a43e5f04826176ed965a983647533
malkh <mohammadalkhaledi2@gmail.com>
2021-10-15 18:11:14 -0400

    PropertySpace updated

--- src/PropertySpace.java
+++ src/PropertySpace.java
@@ -1,3 +1,12 @@
 public class PropertySpace extends Space {
+    public Property getProperty(){
+        return null;
+    }
+    public void onEndTurn(Player p){
+
+    }
+    public String getDescription(){
+        return "";
+    }
 
 }
