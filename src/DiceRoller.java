000c924247c6b64997c69f6d3ec8ea0eb8adee58
wmajbour <wmajbour@hotmail.com>
2021-10-17 14:47:58 -0400

    DiceRoller

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -15,19 +15,34 @@ public class DiceRoller {
 
     private Random roll;
 
+    /**
+     * Rolls the dice.
+     */
+
     public DiceRoller() {
         roll();
     }
 
+    /**
+     * Declares die1 and die2.
+     */
     public void roll() {
         die1 = (int) (Math.random() * 6) + 1;
         die2 = (int) (Math.random() * 6) + 1;
     }
 
+    /**
+     * Calculate the total roll of both dice.
+     * @return sum of die1 and die2
+     */
     public int getTotal() {
         return die1 + die2;
     }
 
+    /**
+     * Check if both dice rolled the same number.
+     * @return true if die1 and die2 are equal.
+     */
     public boolean isDouble(){
         return die1 == die2;
     }
