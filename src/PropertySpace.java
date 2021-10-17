a511a549abe4ca9f64164c6f52c28dd568adabc9
wmajbour <wmajbour@hotmail.com>
2021-10-17 14:44:53 -0400

    DiceRoller

--- src/PropertySpace.java
+++ src/PropertySpace.java
@@ -1,4 +1,14 @@
 public class PropertySpace extends Space {
+
+    /**
+     * Constructor for an abstract Space with only a name.
+     *
+     * @param name the name that should be displayed on this space
+     */
+    public PropertySpace(String name) {
+        super(name);
+    }
+
     public Property getProperty(){
         return null;
     }
