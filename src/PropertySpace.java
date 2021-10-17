a4740e0512e75660008c164036f057534e1ce791
malkh <mohammadalkhaledi2@gmail.com>
2021-10-17 14:15:07 -0400

    Implemented all methods in PropertySpace

--- src/PropertySpace.java
+++ src/PropertySpace.java
@@ -1,12 +1,39 @@
+/**This class implements the property space class, which extends the space class
+ * Milestone 1
+ * @author Mohammad Alkhaledi
+ * */
 public class PropertySpace extends Space {
+    private Property property;
+    /**Default constructor for propertySpace
+     * @param property property for the space
+     * */
+    public PropertySpace(Property property){
+        super(property.getName());
+        this.property = property;
+    }
+    /**Returns property of the property space
+     * @return property
+     * */
     public Property getProperty(){
-        return null;
+        return property;
     }
-    public void onEndTurn(Player p){
 
+    /**
+     * Resolves forced effects resulting from player landing on the space
+     * such as having to pay rent, or landing on the "go to jail" space etc.
+     * @param p the Player landing on this space
+     */
+    public void onEndTurn(Player p){
+        // if player is not the owner of property and property has an owner
+        if(!(p.equals(property.getOwner())) && (property.getOwner() != null)) {
+            p.payRent(property);
+        }
     }
+    /**Returns description of property space
+     * @return property.toString()
+     * */
     public String getDescription(){
-        return "";
+        return property.toString();
     }
 
 }
