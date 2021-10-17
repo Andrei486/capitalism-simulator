530e2301d396138c10ca6948baeb4af7c28a8c1b
wmajbour <wmajbour@hotmail.com>
2021-10-17 14:35:35 -0400

    DiceRoller

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -2,7 +2,8 @@ import java.util.Random;
 
 /**
  * Using the Random util library to simulate rolling dice with a limit of 1 - 6 per die.
- * @author  Waleed Majbour (101144882)
+ *
+ * @author Waleed Majbour (101144882)
  */
 
 public class DiceRoller {
@@ -14,37 +15,47 @@ public class DiceRoller {
 
     private Random roll;
 
-    public DiceRoller(){
+    public DiceRoller() {
         roll();
     }
 
     private void roll() {
-        die1 = (int)(Math.random()*6) + 1;
-        die2 = (int)(Math.random()*6) + 1;
+        die1 = (int) (Math.random() * 6) + 1;
+        die2 = (int) (Math.random() * 6) + 1;
     }
 
-    public int getDie1(){
+    public int getDie1() {
         return die1;
     }
 
-    public int getDie2(){
+    public int getDie2() {
         return die2;
     }
 
-    public int getTotal(){
+    public int getTotal() {
         return die1 + die2;
     }
 
-    public String toString(){
+    public void isDouble(){
+        if(die1 == die2){
+            System.out.print("Player plays again.");
+        }
+    }
+
+    public String toString() {
         return "Player Moves " + getTotal() + " spaces.";
     }
 
     public static void main(String[] args) {
+
         DiceRoller dice;
         int rollCount;
 
         dice = new DiceRoller();
         dice.roll();
+
         System.out.println(dice.getDie1() + " and " + dice.getDie2());
+
+        dice.isDouble();
     }
 }
