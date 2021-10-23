3aa95d3dd3196b65a3c3b4ef42740d7a3834c7ff
malkh <mohammadalkhaledi2@gmail.com>
2021-10-23 16:08:40 -0400

    Implemented parseCommand, handlePayRent and handleBankruptcy. Please review changes made.

--- src/TextController.java
+++ src/TextController.java
@@ -1,3 +1,158 @@
-public class TextController {
+import java.util.HashSet;
+
+public class TextController implements GameEventListener {
+
+    @Override
+    public void handlePayRent(RentEvent e) {
+        Player currentPlayer = board.getCurrentPlayer();
+        Property currentProperty = board.getProperties()[currentPlayer.getPosition()];
+        System.out.println(currentProperty.getName() + " paid $" + currentProperty.getRent() +
+                 " in rent to " + currentProperty.getOwner() + ".");
+
+    }
+
+    @Override
+    public void handleBankruptcy(BankruptcyEvent e) {
+        Player currentPlayer = board.getCurrentPlayer();
+        System.out.println(currentPlayer.getName() + "went bankrupt.");
+    }
+
+
+    /**
+     * @param command a valid command
+     * @author Mohammad Alkhaledi
+     * Receives a command string, and performs appropriate action
+     * list of commands: help, buy, show, show [player name], properties,property [playr name], end
+     */
+    private void parseCommand(String command) {
+        String[] splitCommand = command.split("\\s");
+        command = splitCommand[0];
+        String secondWord = null;
+        if (splitCommand.length > 1) {
+            secondWord = splitCommand[1];
+        }
+        switch (command) {
+            case "help":
+                printHelp();
+                break;
+
+            case "buy":
+                Player currentPlayer = board.getCurrentPlayer();
+                EmptySpace emptySpace = board.getCurrentSpace();
+
+                if (emptySpace.getName().equals("")) {
+                    System.out.println("You are not on a property. You cannot buy right now.");
+                    break;
+                }
+
+                PropertySpace currentSpace = board.getCurrentSpace();
+                Property currentProperty = currentSpace.getProperty();
+                if (currentProperty.getOwner() == currentPlayer) {
+                    System.out.println("This property is owned already. You cannot buy it again.");
+                } else {
+                    currentPlayer.buy(currentProperty);
+                    System.out.println("You pay $" + currentProperty
+                            .getCost() + ", and now own " + currentProperty.getName() + "Congratulation!");
+                    System.out.println("You now have $" + currentPlayer.getMoney() + " remaining.");
+                }
+
+                break;
+
+            case "show":
+                //print a player's stats
+                if (secondWord != null) {
+                    //get the player corresponding to player name, then print stats
+                    Player[] players = board.getPlayers();
+                    Player player1 = null;
+                    for (Player p : players) {
+                        if (secondWord.equals(p.getName())) {
+                            player1 = p;
+                        }
+                    }
+                    if (player1 == null) {
+                        System.out.println("There is no player named " + secondWord);
+                        break;
+                    }
+                    System.out.println("Player name: " + player1.getName());
+                    System.out.println("Money: $" + player1.getMoney());
+                    System.out.print("Properties: ");
+                    HashSet<Property> properties = player1.getProperties();
+                    int propertySize = properties.size();
+                    int counter = 0;
+
+                    for (Property p : properties) {
+                        if (counter < propertySize) {
+                            System.out.print(p.getName() + ", ");
+                        } else {
+                            System.out.print(p.getName() + "\n");
+                        }
+                        ++counter;
+                    }
+
+                }
+
+                //show: print all player's names
+                else {
+                    System.out.println("Print names:");
+                    Player[] players = board.getPlayers();
+                    for (Player p : players) {
+                        System.out.println(p.getName());
+                    }
+                }
+
+                break;
+            case "properties":
+                System.out.print("Properties: ");
+                HashSet<Property> properties = board.getCurrentPlayer().getProperties();
+                int propertySize = properties.size();
+                int counter = 0;
+
+                for (Property p : properties) {
+                    if (counter < propertySize) {
+                        System.out.print(p.getName() + ", ");
+                    } else {
+                        System.out.print(p.getName() + "\n");
+                    }
+                    ++counter;
+                }
+
+                break;
+
+
+            case "property":
+                Property[] properties1 = board.getProperties();
+                Property thisProperty = null;
+                if (secondWord != null) {
+                    for (Property p : properties1) {
+                        if (secondWord.equals(p.getName())) {
+                            thisProperty = p;
+                        }
+                    }
+                    if (thisProperty == null) {
+                        System.out.println("there is no property named " + secondWord);
+                        break;
+                    }
+                    System.out.println(thisProperty.toString());
+
+                } else {
+                    System.out.println("You need to put in a property name.");
+                    break;
+                }
+
+                break;
+            case "end":
+                System.out.println("Ending turn.");
+                advanceTurn();
+                break;
+            default:
+                System.out.println("Invalid command. Type help for a command list");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 
 }
