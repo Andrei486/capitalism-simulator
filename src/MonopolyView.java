9dd3a30ed3fb303fdcce9ccc0f62c9051984e91d
Lubbree <lubbree@gmail.com>
2021-10-29 14:07:13 -0400

    EndTurn Controller implemintation, plus documation updates

--- src/MonopolyView.java
+++ src/MonopolyView.java
@@ -1,3 +1,7 @@
+/**
+ * Allows a class to be a view for the game.
+ * @author Sebastian Lionais 101157892
+ */
 public interface MonopolyView extends GameEventListener{
     void handleMovePlayer(Board board, Player player, int oldPosition);
     void handleBuyProperty(Board board, Player player);
